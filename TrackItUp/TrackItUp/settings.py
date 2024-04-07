92290c15babf1244851eabe666926f7ffa6875e9
Vysockis <lukasvysockis1031@gmail.com>
2024-04-07 19:13:52 +0300

    Changed IP

--- TrackItUp/TrackItUp/settings.py
+++ TrackItUp/TrackItUp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.249', 'localhost']
+ALLOWED_HOSTS = ['84.240.51.3', 'localhost']
 
 
 # Application definition
